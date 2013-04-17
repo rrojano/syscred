0b1bb81c45e5b2f389daf59cdf349895318dff25
Luis Zavaleta <Luis Zavaleta@172.72.49.137>
2013-04-16 20:16:12 -0500

    UI

---
+++ src/test/java/mx/uv/syscred/IUAutenticacion.java
@@ -0,0 +1,105 @@
+package mx.uv.syscred;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFormattedTextField;
+import javax.swing.JPasswordField;
+
+
+public class IUAutenticacion extends JFrame {
+
+	private JPanel Contenedor;
+	private JTextField textUsuario;
+	private JPasswordField textContraseña;
+
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try
+				{
+					IUAutenticacion frame = new IUAutenticacion();
+					frame.setVisible(true);
+				} 
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	public IUAutenticacion() 
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 400, 200);
+		Contenedor = new JPanel();
+		Contenedor.setBorder(new EmptyBorder(5, 5, 5, 5));
+		Contenedor.setLayout(null);
+		setContentPane(Contenedor);
+		
+		final JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setBounds(10, 44, 80, 14);
+		Contenedor.add(lblUsuario);
+		
+		final JLabel lblContraseña = new JLabel("Contraseña");
+		lblContraseña.setBounds(10, 78, 80, 14);
+		Contenedor.add(lblContraseña);
+		
+		textContraseña = new JPasswordField();
+		textContraseña.setBounds(100, 75, 130, 20);
+		Contenedor.add(textContraseña);
+		
+		textUsuario = new JTextField();
+		textUsuario.setBounds(100, 41, 130, 20);
+		Contenedor.add(textUsuario);
+		textUsuario.setColumns(10);
+		
+		final JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				System.exit(0);
+			}
+		});
+		btnCancelar.setBounds(141, 106, 89, 23);
+		Contenedor.add(btnCancelar);
+		
+		final JButton btnAcceder = new JButton("Acceder");
+		btnAcceder.addActionListener(new ActionListener() 
+		{			
+			public void actionPerformed(ActionEvent e) 
+			{
+				if((textUsuario.getText().equals("Admin")) && (textContraseña.getText().equals("Admin")))
+				{				
+					IUCredencial IUC = new IUCredencial(); 
+					IUC.show();
+					dispose();
+					
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Error de autenticación");
+					textUsuario.setText("");
+					textContraseña.setText("");					
+				}
+			}
+		});
+		btnAcceder.setBounds(42, 106, 89, 23);
+		Contenedor.add(btnAcceder);
+	}	
+}
---
+++ src/test/java/mx/uv/syscred/IUCredencial.java
@@ -0,0 +1,97 @@
+package mx.uv.syscred;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.io.File;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
+import javax.swing.JSplitPane;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class IUCredencial extends IUAutenticacion {
+
+	private JPanel contentPane;
+
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					IUCredencial frame = new IUCredencial();
+					frame.setVisible(true);
+				} catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	public IUCredencial() 
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 320);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 0, 574, 193);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(10, 11, 281, 171);
+		panel_1.add(lblNewLabel);
+		File cred = new File("Img/i1.jpg");
+		lblNewLabel.setIcon(new ImageIcon(cred.getPath()));
+		
+		JLabel label = new JLabel("");
+		label.setBounds(283, 11, 281, 171);
+		panel_1.add(label);
+		File cred2 = new File("Img/i2.jpg");
+		label.setIcon(new ImageIcon(cred2.getPath()));
+		
+		JButton btnNewButton = new JButton("Buscar Alumno");
+		btnNewButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				IUBuscarAlu IUBA = new IUBuscarAlu();
+				IUBA.show();				
+			}
+		});
+		btnNewButton.setBounds(34, 204, 188, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Modificar Datos Institución");
+		btnNewButton_1.setBounds(236, 204, 188, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Imprimir");
+		btnNewButton_2.setBounds(34, 239, 89, 23);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Salir");
+		btnNewButton_3.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				dispose();
+			}
+		});
+		btnNewButton_3.setBounds(133, 239, 89, 23);
+		contentPane.add(btnNewButton_3);
+	}
+}
